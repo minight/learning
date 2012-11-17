748c77cd3addc34edf0f7e9bcfab2e930f946df2
sean yeoh <sean.yeoh1@gmail.com>
2012-11-18 10:51:45 +1100

    For Loops and Total and Averages Added

---
+++ CppApplication_3/For Loops Total and Average.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   For Loops Total and Average.cpp
+ * Author: S2XY
+ *
+ * Created on 17 November 2012, 8:33 PM
+ */
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main(){
+    int choice = 0, i, total, j ;
+    double avg = 0;
+    
+    cout << "enter a number between 1 and 1776";
+    cin >> choice;
+    
+    while( choice < i || choice > 1776){
+        cout << "please enter a valid value, between 1 and 1776: ";
+        cin >> choice;
+    }
+    for (i = 1776; i> choice ; i--){
+        total = 
+        cout << setw(8) << i;
+        
+    }
+}
