6794a26c77a2379506a31d406737f6da5ea09027
sean yeoh <sean.yeoh1@gmail.com>
2012-11-17 20:25:49 +1100

    Yay, Finished my nested loops

--- CppApplication_3/main.cpp
+++ CppApplication_3/main.cpp
@@ -1,19 +1,27 @@
-/* 
- * File:   main.cpp
- * Author: S2XY
- *
- * Created on 17 November 2012, 8:03 PM
- */
-
-#include <cstdlib>
-
+#include <iostream>
+#include <cmath>
+//Learning Nested Loops or Nested For Loops.
 using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main(){
+    int choice = 0, i , j;
+   
+    cout << "how many digits do you want to use: ";
+    cin >> choice;
+    
+    for(i=1; i <= choice; i++){ //outer loop
+        for(j=1; j <= i; j++){ //inner loop
+            cout << j;
+        }//end inner loop
+        cout << endl;
+    }//end outer loop
+    
+    for(i=choice; i>= 0; i--){
+        for(j=1; j< i; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+    
+    
     return 0;
 }
-
